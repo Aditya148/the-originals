ab4ceeafba9a9031abc493cf3675e2bbdf418e7b
Aditya148 <aditya.gupta0320@gmail.com>
2019-12-03 22:03:16 +0530

    Updates

---
+++ precache-manifest.69f5f6dbb36ef1ae1afd19d39735175c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303374a680ff84d4a16d7fb0b36a7734",
+    "url": "/the-originals/index.html"
+  },
+  {
+    "revision": "650a3dd54fcfd9e8c5a3",
+    "url": "/the-originals/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e1e1fa4509464c58daf9",
+    "url": "/the-originals/static/js/2.c063cdef.chunk.js"
+  },
+  {
+    "revision": "650a3dd54fcfd9e8c5a3",
+    "url": "/the-originals/static/js/main.b1bd3a0c.chunk.js"
+  },
+  {
+    "revision": "27fa44ac153e8abf6824",
+    "url": "/the-originals/static/js/runtime-main.f51d1a01.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/the-originals/static/media/logo.25bf045c.svg"
+  }
+]);
